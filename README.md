58c70762b887905bd0923234dc616bbde8070595
Haashir-Shakeel <david10james99@gmail.com>
2022-08-22 13:59:07 +0500

    template,url and static files configuration

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 #Readme for django-react integration
+
+1. Create Django App
+    django-admin startproject django_project
+2. Create React App
+    npx create-react-app react-app
+3. Move React App to Django directory
+4. Configure Templates Engine
+    cd to react-app directory and run
+        npm run build
+    In Templates of Django settings.py:
+        os.path.join(BASE_DIR, 'react-app/build')
+5. Url path configuration
+    go to urls.py and import TemplateView add a path 
+        from django.views.generic import TemplateView
+        path(''. TemplateView.as_view(template_name='index.html'))
+6. Configure static files
+    In Settings.py add
+        STATICFILES_DIRS = [
+            os.path.join(BASE_DIR, 'react-app/build/static'),
+        ]
+7. 
--- django_react_integration/settings.py
+++ django_react_integration/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'django_react_integration.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'react-app/build'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +121,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'react-app/build/static'),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
